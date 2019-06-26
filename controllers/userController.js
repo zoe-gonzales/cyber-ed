d6115fb9187bf14ede6d7ce688a49b3fbd24c642
zoe-gonzales <zoegonzales03@gmail.com>
2019-06-25 19:13:31 -0600

    adding mongoose to server.js and creating user controller

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const db = require('../models');
+
+module.exports = {
+  getUser(req, res) {
+    db.User
+      .find({ _id: req.params.id })
+      .then(userData => res.json(userData))
+      .catch(err => res.status(422).json(err));
+  },
+  addUser(req, res) {
+    db.User
+      .create(req.body)
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+  updateUser(req, res) {
+    db.User
+      .updateOne(
+        { _id: req.params.id },
+        { $set: req.body },
+      )
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+  deleteUser(req, res) {
+    db.User
+      .find({ _id: req.params.id })
+      .then(userData => userData.remove())
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,11 +11,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cyberdb');
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
