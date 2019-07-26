a66b31256f2acd3d960c90fc703780fb482e767b
zoe-gonzales <zoegonzales03@gmail.com>
2019-07-26 12:14:23 -0600

    adding back in path module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
